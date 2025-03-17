835e335673d54ffadbdb143802353aba58ff0088
211180120-Yu Bingyao <211180120@smail.nju.edu.cn>
2025-03-17 15:33:58 +0800

    ysyxsoc commit

--- perip/bitrev/bitrev.v
+++ perip/bitrev/bitrev.v
@@ -2,7 +2,47 @@ module bitrev (
   input  sck,
   input  ss,
   input  mosi,
-  output miso
+  output reg miso
 );
-  assign miso = 1'b1;
+  // assign miso = 1'b1;
+  reg[7:0] data;
+  reg[7:0] bit_cnt;
+  initial begin
+    data     = 0;
+    bit_cnt = 0;
+    miso    = 1'b1;
+  end
+
+  always @(posedge sck ) begin
+    if(!ss)begin
+      if(bit_cnt <= 7)begin
+        data <= {data[6:0],mosi};
+        bit_cnt <= bit_cnt + 1;
+      end
+      else if(bit_cnt <=15)begin
+        data <= data;
+        bit_cnt <= bit_cnt + 1;
+      end
+    end
+    else begin
+      bit_cnt <= 0;
+      data <= 0;
+    end
+  end
+
+  always @(negedge sck) begin
+    case(bit_cnt)
+      8'd8:   miso <= data[0];
+      8'd9:   miso <= data[1];
+      8'd10:  miso <= data[2];
+      8'd11:  miso <= data[3];
+      8'd12:  miso <= data[4];
+      8'd13:  miso <= data[5];
+      8'd14:  miso <= data[6];
+      8'd15:  miso <= data[7];
+      default miso <= 1'b1;
+        
+    endcase
+  end
+
 endmodule
--- perip/spi/rtl/spi_top_apb.v
+++ perip/spi/rtl/spi_top_apb.v
@@ -48,19 +48,285 @@ assign in_prdata  = data[31:0];
 
 `else
 
+parameter ysyx_24120011_SPITOPAPB_IDLE         = 4'd0;
+parameter ysyx_24120011_SPITOPAPB_SPI          = 4'd1;
+parameter ysyx_24120011_SPITOPAPB_FLASH_DIV    = 4'd2;
+parameter ysyx_24120011_SPITOPAPB_FLASH_TXREG0 = 4'd3;
+parameter ysyx_24120011_SPITOPAPB_FLASH_TXREG1 = 4'd4;
+parameter ysyx_24120011_SPITOPAPB_FLASH_SS1    = 4'd5;
+parameter ysyx_24120011_SPITOPAPB_FLASH_CTRL   = 4'd6;
+parameter ysyx_24120011_SPITOPAPB_FLASH_WAIT   = 4'd7;
+parameter ysyx_24120011_SPITOPAPB_FLASH_SS0    = 4'd8;
+parameter ysyx_24120011_SPITOPAPB_FLASH_RECV   = 4'd9;
+
+reg [3:0] spi_top_apb_state;
+reg [3:0] spi_top_apb_nextstate;
+
+//in
+reg [31:0] reg_in_paddr;
+reg [31:0] reg_in_pwdata;
+reg [3:0]  reg_in_pstrb;
+reg reg_in_pwrite;
+reg reg_in_psel;
+reg reg_in_penable;
+//out
+reg [31:0] reg_in_prdata;
+reg reg_in_pready;
+reg reg_in_pslverr;
+reg reg_spi_irq_out;
+
+assign in_pslverr = reg_in_pslverr;
+assign spi_irq_out = reg_spi_irq_out;
+assign in_pready = (spi_top_apb_state == ysyx_24120011_SPITOPAPB_SPI || spi_top_apb_state == ysyx_24120011_SPITOPAPB_FLASH_RECV) ? reg_in_pready : 'd0;
+assign in_prdata = (spi_top_apb_state == ysyx_24120011_SPITOPAPB_SPI) ? reg_in_prdata : (spi_top_apb_state == ysyx_24120011_SPITOPAPB_FLASH_RECV) ? {reg_in_prdata[7:0],reg_in_prdata[15:8],reg_in_prdata[23:16],reg_in_prdata[31:24]}:'d0;
+
+always@(*) begin
+  case(spi_top_apb_state)
+    ysyx_24120011_SPITOPAPB_IDLE  : begin
+      if(in_psel & (in_paddr[31:28] == 4'h1))begin
+        reg_in_paddr   = 'd0;
+        reg_in_pwdata  = 'd0;
+        reg_in_pstrb   = 'd0;
+        reg_in_pwrite  = 'd0;
+        reg_in_psel    = 'd0;
+        reg_in_penable = 'd0;
+				spi_top_apb_nextstate	= ysyx_24120011_SPITOPAPB_SPI;
+			end
+			else if(in_psel & (in_paddr[31:28] == 4'h3))begin
+        reg_in_paddr   = 'd0;
+        reg_in_pwdata  = 'd0;
+        reg_in_pstrb   = 'd0;
+        reg_in_pwrite  = 'd0;
+        reg_in_psel    = 'd0;
+        reg_in_penable = 'd0;
+				spi_top_apb_nextstate	= ysyx_24120011_SPITOPAPB_FLASH_DIV;
+			end
+      else begin
+        reg_in_paddr   = 'd0;
+        reg_in_pwdata  = 'd0;
+        reg_in_pstrb   = 'd0;
+        reg_in_pwrite  = 'd0;
+        reg_in_psel    = 'd0;
+        reg_in_penable = 'd0;
+        spi_top_apb_nextstate	= ysyx_24120011_SPITOPAPB_IDLE;
+      end
+    end
+    ysyx_24120011_SPITOPAPB_SPI   : begin
+      if(reg_in_pready) begin
+        reg_in_paddr   = in_paddr   ;
+        reg_in_pwdata  = in_pwdata  ;
+        reg_in_pstrb   = in_pstrb   ;
+        reg_in_pwrite  = in_pwrite  ;
+        reg_in_psel    = in_psel    ;
+        reg_in_penable = in_penable ;
+        spi_top_apb_nextstate = ysyx_24120011_SPITOPAPB_IDLE;
+      end
+      else begin
+        reg_in_paddr   = in_paddr   ;
+        reg_in_pwdata  = in_pwdata  ;
+        reg_in_pstrb   = in_pstrb   ;
+        reg_in_pwrite  = in_pwrite  ;
+        reg_in_psel    = in_psel    ;
+        reg_in_penable = in_penable ;
+        spi_top_apb_nextstate = ysyx_24120011_SPITOPAPB_SPI;
+      end
+    end
+    ysyx_24120011_SPITOPAPB_FLASH_DIV : begin
+      if(reg_in_pready) begin
+        reg_in_paddr   =  32'h00000014   ;
+        reg_in_pwdata  =  32'h0000000f   ;
+        reg_in_pstrb   =  4'hf           ;
+        reg_in_pwrite  = 'd0             ;
+        reg_in_psel    = 'd0             ;
+        reg_in_penable = 'd0             ;
+        spi_top_apb_nextstate = ysyx_24120011_SPITOPAPB_FLASH_TXREG0;
+      end
+      else begin
+        reg_in_paddr   =  32'h00000014   ;
+        reg_in_pwdata  =  32'h0000000f   ;
+        reg_in_pstrb   =  4'hf           ;
+        reg_in_pwrite  = 'd1             ;
+        reg_in_psel    = 'd1             ;
+        reg_in_penable = 'd1             ;
+        spi_top_apb_nextstate = ysyx_24120011_SPITOPAPB_FLASH_DIV;
+      end
+    end
+    ysyx_24120011_SPITOPAPB_FLASH_TXREG0 : begin
+      if(reg_in_pready) begin
+        reg_in_paddr   =  32'h00000000   ;
+        reg_in_pwdata  =  32'h00000000   ;
+        reg_in_pstrb   =  4'hf           ;
+        reg_in_pwrite  = 'd0             ;
+        reg_in_psel    = 'd0             ;
+        reg_in_penable = 'd0             ;
+        spi_top_apb_nextstate = ysyx_24120011_SPITOPAPB_FLASH_TXREG1;
+      end
+      else begin
+        reg_in_paddr   =  32'h00000000   ;
+        reg_in_pwdata  =  32'h00000000   ;
+        reg_in_pstrb   =  4'hf           ;
+        reg_in_pwrite  = 'd1             ;
+        reg_in_psel    = 'd1             ;
+        reg_in_penable = 'd1             ;
+        spi_top_apb_nextstate = ysyx_24120011_SPITOPAPB_FLASH_TXREG0;
+      end
+    end
+    ysyx_24120011_SPITOPAPB_FLASH_TXREG1 : begin
+      if(reg_in_pready) begin
+        reg_in_paddr   =  32'h00000004   ;
+        reg_in_pwdata  =  {8'h03,in_paddr[23:0]};
+        reg_in_pstrb   =  4'hf           ;
+        reg_in_pwrite  = 'd0             ;
+        reg_in_psel    = 'd0             ;
+        reg_in_penable = 'd0             ;
+        spi_top_apb_nextstate = ysyx_24120011_SPITOPAPB_FLASH_SS1;
+      end
+      else begin
+        reg_in_paddr   =  32'h00000004   ;
+        reg_in_pwdata  =  {8'h03,in_paddr[23:0]};
+        reg_in_pstrb   =  4'hf           ;
+        reg_in_pwrite  = 'd1             ;
+        reg_in_psel    = 'd1             ;
+        reg_in_penable = 'd1             ;
+        spi_top_apb_nextstate = ysyx_24120011_SPITOPAPB_FLASH_TXREG1;
+      end
+    end
+    ysyx_24120011_SPITOPAPB_FLASH_SS1 : begin
+      if(reg_in_pready) begin
+        reg_in_paddr   =  32'h00000018   ;
+        reg_in_pwdata  =  32'h00000001   ;
+        reg_in_pstrb   =  4'hf           ;
+        reg_in_pwrite  = 'd0             ;
+        reg_in_psel    = 'd0             ;
+        reg_in_penable = 'd0             ;
+        spi_top_apb_nextstate = ysyx_24120011_SPITOPAPB_FLASH_CTRL;
+      end
+      else begin
+        reg_in_paddr   =  32'h00000018   ;
+        reg_in_pwdata  =  32'h00000001   ;
+        reg_in_pstrb   =  4'hf           ;
+        reg_in_pwrite  = 'd1             ;
+        reg_in_psel    = 'd1             ;
+        reg_in_penable = 'd1             ;
+        spi_top_apb_nextstate = ysyx_24120011_SPITOPAPB_FLASH_SS1;
+      end
+    end
+    ysyx_24120011_SPITOPAPB_FLASH_CTRL : begin
+      if(reg_in_pready) begin
+        reg_in_paddr   =  32'h00000010   ;
+        reg_in_pwdata  =  32'h00001140   ;
+        reg_in_pstrb   =  4'hf           ;
+        reg_in_pwrite  = 'd0             ;
+        reg_in_psel    = 'd0             ;
+        reg_in_penable = 'd0             ;
+        spi_top_apb_nextstate = ysyx_24120011_SPITOPAPB_FLASH_WAIT;
+      end
+      else begin
+        reg_in_paddr   =  32'h00000010   ;
+        reg_in_pwdata  =  32'h00001140   ;
+        reg_in_pstrb   =  4'hf           ;
+        reg_in_pwrite  = 'd1             ;
+        reg_in_psel    = 'd1             ;
+        reg_in_penable = 'd1             ;
+        spi_top_apb_nextstate = ysyx_24120011_SPITOPAPB_FLASH_CTRL;
+      end
+    end
+    ysyx_24120011_SPITOPAPB_FLASH_WAIT : begin
+      if(reg_spi_irq_out) begin
+        reg_in_paddr   = 'd0             ;
+        reg_in_pwdata  = 'd0             ;
+        reg_in_pstrb   = 'd0             ;
+        reg_in_pwrite  = 'd0             ;
+        reg_in_psel    = 'd0             ;
+        reg_in_penable = 'd0             ;
+        spi_top_apb_nextstate = ysyx_24120011_SPITOPAPB_FLASH_SS0;
+      end
+      else begin
+        reg_in_paddr   = 'd0             ;
+        reg_in_pwdata  = 'd0             ;
+        reg_in_pstrb   = 'd0             ;
+        reg_in_pwrite  = 'd0             ;
+        reg_in_psel    = 'd0             ;
+        reg_in_penable = 'd0             ;
+        spi_top_apb_nextstate = ysyx_24120011_SPITOPAPB_FLASH_WAIT;
+      end
+    end
+    ysyx_24120011_SPITOPAPB_FLASH_SS0 : begin
+      if(reg_in_pready) begin
+        reg_in_paddr   =  32'h00000018   ;
+        reg_in_pwdata  =  32'h00000000   ;
+        reg_in_pstrb   =  4'hf           ;
+        reg_in_pwrite  = 'd0             ;
+        reg_in_psel    = 'd0             ;
+        reg_in_penable = 'd0             ;
+        spi_top_apb_nextstate = ysyx_24120011_SPITOPAPB_FLASH_RECV;
+      end
+      else begin
+        reg_in_paddr   =  32'h00000018   ;
+        reg_in_pwdata  =  32'h00000000   ;
+        reg_in_pstrb   =  4'hf           ;
+        reg_in_pwrite  = 'd1             ;
+        reg_in_psel    = 'd1             ;
+        reg_in_penable = 'd1             ;
+        spi_top_apb_nextstate = ysyx_24120011_SPITOPAPB_FLASH_SS0;
+      end
+    end
+    ysyx_24120011_SPITOPAPB_FLASH_RECV : begin
+      if(reg_in_pready) begin
+        reg_in_paddr   =  32'h00000000   ;
+        reg_in_pwdata  =  32'h00000000   ;
+        reg_in_pstrb   =  4'hf           ;
+        reg_in_pwrite  = 'd0             ;
+        reg_in_psel    = 'd0             ;
+        reg_in_penable = 'd0             ;
+        spi_top_apb_nextstate = ysyx_24120011_SPITOPAPB_IDLE;
+      end
+      else begin
+        reg_in_paddr   =  32'h00000000   ;
+        reg_in_pwdata  =  32'h00000000   ;
+        reg_in_pstrb   =  4'hf           ;
+        reg_in_pwrite  = 'd0             ;
+        reg_in_psel    = 'd1             ;
+        reg_in_penable = 'd1             ;
+        spi_top_apb_nextstate = ysyx_24120011_SPITOPAPB_FLASH_RECV;
+      end
+    end
+    default: begin
+      reg_in_paddr   = 'd0;
+      reg_in_pwdata  = 'd0;
+      reg_in_pstrb   = 'd0;
+      reg_in_pwrite  = 'd0;
+      reg_in_psel    = 'd0;
+      reg_in_penable = 'd0;
+      spi_top_apb_nextstate = ysyx_24120011_SPITOPAPB_IDLE;
+    end
+  endcase
+end
+
+
+
+always@(posedge clock) begin
+  if(reset) begin
+    spi_top_apb_state <= ysyx_24120011_SPITOPAPB_IDLE;
+  end
+  else begin
+    spi_top_apb_state <= spi_top_apb_nextstate;
+  end
+end
+
 spi_top u0_spi_top (
   .wb_clk_i(clock),
   .wb_rst_i(reset),
-  .wb_adr_i(in_paddr[4:0]),
-  .wb_dat_i(in_pwdata),
-  .wb_dat_o(in_prdata),
-  .wb_sel_i(in_pstrb),
-  .wb_we_i (in_pwrite),
-  .wb_stb_i(in_psel),
-  .wb_cyc_i(in_penable),
-  .wb_ack_o(in_pready),
-  .wb_err_o(in_pslverr),
-  .wb_int_o(spi_irq_out),
+  .wb_adr_i(reg_in_paddr[4:0]),
+  .wb_dat_i(reg_in_pwdata),
+  .wb_dat_o(reg_in_prdata),
+  .wb_sel_i(reg_in_pstrb),
+  .wb_we_i (reg_in_pwrite),
+  .wb_stb_i(reg_in_psel),
+  .wb_cyc_i(reg_in_penable),
+  .wb_ack_o(reg_in_pready),
+  .wb_err_o(reg_in_pslverr),
+  .wb_int_o(reg_spi_irq_out),
 
   .ss_pad_o(spi_ss),
   .sclk_pad_o(spi_sck),
