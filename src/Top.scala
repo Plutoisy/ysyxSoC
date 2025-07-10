33b6dac821097edb3de9d9850b2bb7cbe372793f
211180120-Yu Bingyao <211180120@smail.nju.edu.cn>
2025-07-10 23:10:37 +0800

    sdram的apb改axi

--- src/Top.scala
+++ src/Top.scala
@@ -7,7 +7,7 @@ import freechips.rocketchip.diplomacy.LazyModule
 
 object Config {
   def hasChipLink: Boolean = true
-  def sdramUseAXI: Boolean = false
+  def sdramUseAXI: Boolean = true
 }
 
 class ysyxSoCTop extends Module {
